dbe970c152028eaefa66b5770045f5da5b5ef9b8
ThinkPad Korra <hita999@live.fr>
2024-04-09 09:52:22 +0200

    After pull

---
+++ Doctolib_Back_Office (Last version)/src/controller/tableController.java
@@ -0,0 +1,75 @@
+package controller;
+
+import javax.swing.table.AbstractTableModel;
+
+public class tableController extends AbstractTableModel{
+
+	private String header []; 
+	private Object data[][]; 
+	
+	public tableController(String[] header, Object[][] data) {
+		super();
+		this.setHeader(header);
+		this.setData(data);
+	}
+
+	public String[] getHeader() {
+		return header;
+	}
+
+	public void setHeader(String header[]) {
+		this.header = header;
+	}
+
+	public Object[][] getData() {
+		return data;
+	}
+
+	public void setData(Object data[][]) {
+		this.data = data;
+	}
+
+	
+	public int getRowCount() {
+		 
+		return this.data.length; 
+	}
+
+	
+	public int getColumnCount() {
+		 
+		return this.header.length; 
+	}
+
+	
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		 
+		return this.data[rowIndex][columnIndex];
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		return this.header[column];
+	}
+
+	public int getSelectedRow() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public void removeRow(int row) {
+		Object matrice [][] = new Object [this.data.length-1][this.header.length];
+		//on recopie les donnees dans la matrice 
+		int j = 0; 
+		for (int i=0; i <this.data.length; i++) {
+			if(i != row) {
+					matrice [j] = this.data[i]; 
+					j++;
+			}
+		}
+		//on ecrase les données par la nouvelle matrice
+		this.data = matrice; 
+		//on applique les changements 
+		this.fireTableDataChanged();
+	}
+	
+}
---
+++ Doctolib_Back_Office (Last version)/src/model/DeleteModel.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import controller.UserController;
+
+public class DeleteModel {
+	private static DBConnection bdd = new DBConnection ("localhost:3306","btsProject","phpmyadmin","root");
+	
+	public static void DeleteUser(int idUser) {
+		String requete = "DELETE FROM btsProject_User WHERE idUser = "+idUser+";";
+		try {
+			bdd.Connect();
+			//création d'un curseur pour exécuter la requete 
+			Statement req = bdd.getMaConnexion().createStatement(); 
+			//execution de la requete et récuperation d'un resultat 
+			ResultSet data = req.executeQuery(requete); 
+			//s'il y a un resultat, on récupere les champs 
+			req.close();
+			bdd.Disconnect();
+		}
+		catch (SQLException exp) {
+			System.out.println("Erreur de requete : " +requete);
+		}
+	}
+	
+	
+
+}
