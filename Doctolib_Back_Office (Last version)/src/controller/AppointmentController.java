37c37a1689a787ce44838cd715facbfa82889bdb
Korra <hita999@live.fr>
2024-04-30 16:48:05 +0200

    In work

---
+++ Doctolib_Back_Office (Last version)/src/controller/AppointmentController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class AppointmentController {
+	private int idAppointment;
+	private String dateAppointment, timeAppointment, reason, idPatient, idDoctor;
+	public AppointmentController(int idAppointment, String dateAppointment, String timeAppointment, String reason, String ) {
+		
+	}
+}
